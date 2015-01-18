5b2b5844c70b828350cbe1035a7d660914ad8b68
Chuck Lauer Vose <vosechu@gmail.com>
2015-01-18 11:42:29 -0800

    add notes to the readme

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Learn the basics of what a webserver actually does and how it turns
 markdown into html dynamically and stores the data on the filesystem
 for caching or to be used by a more purpose-built webserver
 
+**Important Note:** The code in this repo is not safe to use in production.
+It is full of security holes, it's not optimized, and generally shouldn't
+be used! Please consider using Sinatra, Rails, Jekyll, Flask, or basically
+anything else. This is for teaching purposes only.
+
 # Main topics we'll cover
 
 * Netcat
@@ -27,6 +32,16 @@ bundle install
 bundle exec shotgun
 ```
 
-For each step, change the config.ru to the next HelloWorld and use the url:
+For each step, uncomment the next step in the app.rb and comment the
+section above it (there should only be one uncommented `class HelloWorld`
+at any time).
+
+Now use your browser to request this url:
 'http://localhost:9393/?person=Erica&q=test'
 
+## Step 8
+
+Uncomment the last section of the app.rb and read through the comments. If
+you can figure out how to write files to the filesystem, you will have
+mostly recreated Jekyll (minus some lovely and important things like
+security, speed, generation scripts, bloginess, ...)
--- app.rb
+++ app.rb
@@ -1,90 +1,97 @@
+# Step 2
 class HelloWorld
   def call(env)
     [200, {}, ['Hello World']]
   end
 end
 
-class HelloWorld2
-  def call(env)
-    # Please, please, don't put params directly into the output
-    # like this. It's not safe from hackers!
-    [200, {}, ["Hello, #{env['QUERY_STRING']}"]]
-  end
-end
+# # Step 3
+# class HelloWorld
+#   def call(env)
+#     # Please, please, don't put params directly into the output
+#     # like this. It's not safe from hackers!
+#     [200, {}, ["Hello, #{env['QUERY_STRING']}"]]
+#   end
+# end
 
-class HelloWorld3
-  def call(env)
-    request = Rack::Request.new env
+# # Step 4
+# class HelloWorld
+#   def call(env)
+#     request = Rack::Request.new env
 
-    # Please, please, don't put params directly into the output
-    # like this. It's not safe from hackers!
-    [200, {}, ["Hello, #{request.params['person']}"]]
-  end
-end
+#     # Please, please, don't put params directly into the output
+#     # like this. It's not safe from hackers!
+#     [200, {}, ["Hello, #{request.params['person']}"]]
+#   end
+# end
 
-class HelloWorld4
-  def call(env)
-    request = Rack::Request.new env
-    # Please, please, don't open files blindly like this upon request
-    # from an external user. It's not safe from hackers!
-    file = File.open(request.params['q'] + '.html')
+# # Step 5
+# class HelloWorld
+#   def call(env)
+#     request = Rack::Request.new env
+#     # Please, please, don't open files blindly like this upon request
+#     # from an external user. It's not safe from hackers!
+#     file = File.open(request.params['q'] + '.html')
 
-    [200, {}, file]
-  end
-end
+#     [200, {}, file]
+#   end
+# end
 
-class HelloWorld5
-  def call(env)
-    request = Rack::Request.new env
-    @person = request.params['person']
+# # Step 6
+# class HelloWorld
+#   def call(env)
+#     request = Rack::Request.new env
+#     @person = request.params['person']
 
-    # Please, please, just don't do anything in this file. Use sinatra
-    # or rails to do this stuff. They're smarter than us! :)
-    file = File.open(request.params['q'] + '.html.erb')
-    template = ERB.new(file.read)
-    result = template.result(binding)
+#     # Please, please, just don't do anything in this file. Use sinatra
+#     # or rails to do this stuff. They're smarter than us! :)
+#     file = File.open(request.params['q'] + '.html.erb')
+#     template = ERB.new(file.read)
+#     result = template.result(binding)
 
-    [200, {}, [result]]
-  end
-end
+#     [200, {}, [result]]
+#   end
+# end
 
-class HelloWorld6
-  def call(env)
-    request = Rack::Request.new env
-    @person = request.params['person']
+# # Step 7
+# class HelloWorld
+#   def call(env)
+#     request = Rack::Request.new env
+#     @person = request.params['person']
 
-    # This one is actually pretty safe. As we go up to higher-level and
-    # more complex gems they start to do more things like sanitization.
-    file = request.params['q'] + '.md'
-    require 'github/markup'
-    @page = GitHub::Markup.render(file, File.open(file).read)
+#     # This one is actually pretty safe. As we go up to higher-level and
+#     # more complex gems they start to do more things like sanitization.
+#     file = request.params['q'] + '.md'
+#     require 'github/markup'
+#     @page = GitHub::Markup.render(file, File.open(file).read)
 
-    # Please, please, just don't do anything in this file. Use sinatra
-    # or rails to do this stuff. They're smarter than us! :)
-    layout = File.open('layout.html.erb')
-    template = ERB.new(layout.read)
-    result = template.result(binding)
+#     # Please, please, just don't do anything in this file. Use sinatra
+#     # or rails to do this stuff. They're smarter than us! :)
+#     layout = File.open('layout.html.erb')
+#     template = ERB.new(layout.read)
+#     result = template.result(binding)
 
-    [200, {}, [result]]
-  end
-end
+#     [200, {}, [result]]
+#   end
+# end
 
-class HelloWorld7
-  def call(env)
-    # Initial creation steps ("caching")
-      # Make a request object
-      # Find params, sanitize them and pass them to a template tool like
-      # markdown or ERB or github-markup
-      # Make the layout, pass in the page object we just created
-      # Save the plain html file to a folder named something like _site
-    # Now we just need to go back to HelloWorld4
-      # Or even better, use Apache or Nginx which are /much/ better at
-      # serving plain html pages.
-    # This is called caching, but we could do this without requesting
-    # webpages at all. This is what `jekyll build` does. So all you get
-    # is plain html pages with no need to boot a ruby server at all. This
-    # is what github-pages does (ie: username.github.io)
+# # Step 8
+# class HelloWorld
+#   def call(env)
+#     # Initial creation steps ("caching")
+#       # Make a request object
+#       # Find params, sanitize them and pass them to a template tool like
+#       # markdown or ERB or github-markup
+#       # Make the layout, pass in the page object we just created
+#       # Save the plain html file to a folder named something like _site
+#     # Now we just need to go back to HelloWorld4
+#       # Or even better, use Apache or Nginx which are /much/ better at
+#       # serving plain html pages.
+#     # This is called caching, but we could do this without requesting
+#     # webpages at all. This is what `jekyll build` does. So all you get
+#     # is plain html pages with no need to boot a ruby server at all. This
+#     # is what github-pages does (ie: username.github.io)
 
-    [200, {}, ['']]
-  end
-end
+#     [200, {}, ['']]
+#   end
+# end
